e37d51d287d653f5b10f418b06b3d2688bb42736
StephanLoecher <stephan.loecher@skyered.de>
2025-11-25 11:55:46 +0100

    removed dist folder

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("./utils");
-const a = 10;
-const b = 5;
-console.log(`Add: ${(0, utils_1.add)(a, b)}`);
-console.log(`Subtract: ${(0, utils_1.subtract)(a, b)}`);
